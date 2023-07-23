288a5a04759d24337f0949c7b47c6b818fa38254
ChristianG98A <cristi.hva@gmail.com>
2023-07-23 21:18:21 +0300

    Datagrid product results

--- src/app/receptie_marfa/comenzi_furnizori/editare_nir/page.tsx
+++ src/app/receptie_marfa/comenzi_furnizori/editare_nir/page.tsx
@@ -2,12 +2,12 @@
 
 import {StateContext} from "@/app/state/context";
 import {PageBreadcrumbs} from "@/components/features/PageBreadcrumbs";
-import {Autocomplete, Box, Button, Divider, FormControl, Grid, InputLabel, MenuItem, Select, SelectChangeEvent, TextField, Typography, useStepContext} from "@mui/material";
+import {Box, Button, Divider, Grid, TextField, Typography} from "@mui/material";
 import {useContext, useEffect, useState} from "react";
 import {useForm} from "react-hook-form";
 import {useRouter} from "next/navigation";
 import {callNextApi} from "@/helpers/apiMethods";
-const _ = require('lodash');
+import {DataGrid, GridColDef, GridToolbar} from "@mui/x-data-grid";
 
 const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
     if (reason === 'clickaway') {
@@ -21,34 +21,42 @@ const InvoiceEdit = () => {
     const {register, handleSubmit} = useForm();
     const router = useRouter();
     const [loading, setLoading] = useState(false);
+    const [selectedProduct, setSelectedProduct] = useState<string>();
+    const [inputValue, setInputValue] = useState('');
+    const debounce = require('lodash.debounce');
+    const [selectionModel, setSelectionModel] = useState<any>({})
+    const [paginationModel, setPaginationModel] = useState({
+        pageSize: 10,
+        page: 0
+    })
     //const [allProducts, setAllProducts] = useState<any>();
 
-    const handleSearch = _.debounce((event: React.ChangeEvent<HTMLInputElement>) => {
-        console.log(event.target.value)
-        callNextApi("POST", "purchase/searchProduct", {searchterm: event.target.value})
-        .catch(e=>console.log("Error searching for product: ", e))
-        .then((r:any)=>{
-            console.log(r.response)
-                let id_key =1
-                const result=r.response.map((product:{id:string, name:string})=>({label:`${id_key++}: ${product.name}`}))
-                dispatch({type:"SET_PRODUCT_RESULT", payload:result})
+    const handleSearch = debounce(async (event: React.ChangeEvent<HTMLInputElement>) => {
+        const input = event.target.value;
+        if (input?.length >= 3) {
+        setLoading(true)
+
+            await callNextApi("POST", "purchase/searchProduct", {searchterm: input})
+                .catch(e => console.log("Error searching for product: ", e))
+                .then((r: any) => {
+                    console.log(r.response)
+                    let id_key = 1
+                    const result = r.response.map((product: {id: string, name: string, model: string}) => ({crt: id_key++, ...product}))
+                    dispatch({type: "SET_PRODUCT_RESULT", payload: result})
+                    setLoading(false)
             })
+        }
     }, 1000)
 
-    const allProducts = [
-        {label: 'The Shawshank Redemption', },
-        {label: 'The ssaShawshank Redemption', },
-        {label: 'The ar2rShawshank Redemption', },
-        ]
 
     useEffect(() => {
-        setLoading(true)
         state.currentInvoice ?? router.push('/receptie_marfa/comenzi_furnizori')
     }, [])
 
-    useEffect(()=>console.log("All products\n", state.productResult), [state.productResult])
+    useEffect(()=>console.log("Checkbox Selected: ", selectionModel), [selectionModel])
 
-    return (
+
+  return (
         <>
             <Typography variant={"h6"} sx={{mb: 2}} gutterBottom >{"Lista note intrare-receptie id: " + state.currentInvoice}</ Typography>
             <PageBreadcrumbs
@@ -74,20 +82,42 @@ const InvoiceEdit = () => {
                 p: 4,
             }}>
                 <Typography textAlign={"center"} id="new_invoice_modal" variant="h6" component="h2">
-                    Alege produs
-                </Typography>
-                <Grid alignItems={"center"} justifyItems={"center"} justifyContent={"center"} container spacing={3} flex={3} sx={{mt:5}}>
-                        <Autocomplete
-                            disablePortal
-                            id="product_selection"
-                            sx={{width: "80%"}}
-                            options={state.productResult}
-                            renderInput={(params: any) => {
-                                const { key, id, ...otherParams } = params;
-                                return (<TextField  {...otherParams} onChange={handleSearch} label="Alege Produs" />)
-                            }}
+                  Alege produse
+              </Typography>
+              <Grid alignItems={"center"} justifyItems={"center"} justifyContent={"center"} container spacing={3} flexDirection={"column"} sx={{mt: 5}}>
+                    <Grid item xs={10} sm={10} md={10} lg={10} xl={10} alignItems={"center"} justifyItems={"center"} justifyContent={"center"} >
+                        <TextField
+                        sx={{width:'30rem'}}
+                            onChange={handleSearch}
+                            label={'Denumire / EAN / ID'}
                         />
+                    </ Grid>
+
+                    <Grid item xs={10} sm={10} md={10} lg={10} xl={10}  sx={{width:"80%"}}>
+                        <DataGrid
+                            rowSelection={true}
+                            columnHeaderHeight={60}
+                            checkboxSelection
+                            rows={state.productResult}
+                            pageSizeOptions={[10, 25, 50]}
+                            initialState={{pagination: {paginationModel: paginationModel}}}
+                            onPaginationModelChange={setPaginationModel}
+                            onRowSelectionModelChange={(newRowSelectionModel) => {
+                                setSelectionModel(newRowSelectionModel)
+                            }}
+                            columns={[
+                                {field: 'crt', headerName: 'Crt', flex: 1},
+                                {field: 'id', headerName: 'ID Produs', flex: 1},
+                                {field: 'name', headerName: 'Denumire Produs', flex: 4},
+                                {field: 'model', headerName: 'Model', flex: 4},
+                            ]}
+                            rowSelectionModel={state.selectionModel}
+                            autoPageSize={false}
+                            loading={loading}
+                        />
+                    </ Grid>
                 </Grid>
+
                 <Divider sx={{mt: 10}} />
                 <Box sx={{display: "flex", justifyContent: "right", gap: "5rem", pt: 2}} >
                     <Button variant="contained" type={"submit"}>
